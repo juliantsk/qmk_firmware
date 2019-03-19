5de28bb09fccf1518401c18df68b3e1b6e36ee16
Julian T Skodzinski <juliantsk@gmail.com>
2019-03-19 19:13:42 -0400

    Finished Default Layer.

--- keyboards/xd75/keymaps/juliantsk/keymap.c
+++ keyboards/xd75/keymaps/juliantsk/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
  * | A      | R      | S      | T      | D      | PRINT  | P4     | P5     | P6     | SCROLL | H      | N      | E      | I      | O      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | Z      | X      | C      | V      | B      | INSERT | P3     | P2     | P3     | HOME   | K      | M      | ,      | .      | /      |
+ * | Z      | X      | C      | V      | B      | INSERT | P1     | P2     | P3     | HOME   | K      | M      | ,      | .      | /      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LCTRL  | SUPER  | LALT   | +      | VOLUP  | DELETE | PN     | P0     | P.     | END    | VOLDN  | PGUP   | RALT   | UP     | RCTRL  |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
@@ -42,16 +42,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_QW] = LAYOUT_ortho_5x15( /* COLEMAK */
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC,  KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC,  KC_SPC,  MO(_FN), KC_RALT, KC_RGUI, KC_RCTL
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,     KC_NLCK, KC_P7,   KC_P8,   KC_P9,   KC_MPLY,   KC_J,       KC_L,      KC_U,    KC_Y,    KC_SCLN,
+    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,     KC_PSCR, KC_P4,   KC_P5,   KC_P6,   KC_SLCK,   KC_H,       KC_N,      KC_E,    KC_I,    KC_O,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_INS,  KC_P1,   KC_P2,   KC_P3,   KC_HOME,   KC_K,       KC_M,      KC_COMM, KC_DOT,  KC_SLSH,
+    KC_LCTL, KC_LGUI, KC_LALT, KC_PLUS, KC_VOLUP, KC_DEL,  KC_NO,   KC_P0,   KC_PDOT, KC_END,    KC_VOLDOWN, KC_PGUP,   KC_RALT, KC_UP,   KC_RCTRL,
+    KC_PAST, KC_NO,   KC_NO,   KC_MINS, KC_SPC,   KC_LSFT, MO(_FN), KC_ESC,  MO(_FN), KC_RSFT,   KC_SPC,     KC_PGDOWN, KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
 /* FUNCTION
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | 1      | 2      | 3      | 4      | 5      |        |        |        |        |        | 6      | 7      | 8      | 9      | 10     |
+ * | 1      | 2      | 3      | 4      | 5      |        |        |        |        |        | 6      | 7      | 8      | 9      | 0      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | SELECT | CALC   | MYCOMP | MAIL   | RGB HD | RGB HI | P7     | P8     | P9     | -      |        |        | PR SCR | SCR LK | PAUSE  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
@@ -64,11 +64,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_FN] = LAYOUT_ortho_5x15( /* FUNCTION */
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, ______,  _______, _______,  _____,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,
     KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, RESET,   _______, _______, _______,
     KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______,
-    _______, _______, RGB_TOG, MO(_FN), RGB_RMOD,RGB_MOD, KC_P0,   _______, KC_PDOT, KC_PENT, KC_PENT, MO(_FN), _______, _______, _______
+    _______, _______, RGB_TOG, _______, RGB_RMOD,RGB_MOD, MO(_FN), _______, MO(_FN), KC_PENT, KC_PENT, MO(_FN), _______, _______, _______
   )
 };
 
