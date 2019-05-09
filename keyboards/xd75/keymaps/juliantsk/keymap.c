c1c583d6f532530db71aa3509707457c56f16471
Julian T Skodzinski <juliantsk@gmail.com>
2019-05-08 23:01:44 -0400

    Fixed some layout issues.

--- keyboards/xd75/keymaps/juliantsk/keymap.c
+++ keyboards/xd75/keymaps/juliantsk/keymap.c
@@ -32,11 +32,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | Q      | W      | F      | P      | G      | CAP LK | P7     | P8     | P9     | PAUSE  | J      | L      | U      | Y      | ;      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | A      | R      | S      | T      | D      | PRINT  | P4     | P5     | P6     | SCROLL | H      | N      | E      | I      | O      |
+ * | A      | R      | S      | T      | D      | PRINT  | P4     | P5     | P6     | HOME   | H      | N      | E      | I      | O      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | Z      | X      | C      | V      | B      | ENTER  | P1     | P2     | P3     | HOME   | K      | M      | ,      | .      | /      |
+ * | Z      | X      | C      | V      | B      | SCROLL | P1     | P2     | P3     | END    | K      | M      | ,      | .      | /      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LCTRL  | SUPER  | LALT   | =      | VOLUP  | DELETE | PN     | P0     | P.     | END    | VOLDN  | PGUP   | RALT   | UP     | RCTRL  |
+ * | LCTRL  | SUPER  | LALT   | =      | VOLUP  | ENTER  | NO     | P0     | P.     | BKSPC  | VOLDN  | PGUP   | RALT   | UP     | RCTRL  |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
  * | P*     | COPY   | FN     | -      | SPACE  | SHIFT  | SS     | ESC    | SS     | SHIFT  | SPACE  | PGDN   | LEFT   | DOWN   | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
@@ -44,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_CO] = LAYOUT_ortho_5x15( /* COLEMAK */
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_CAPS, KC_P7,   KC_P8,   KC_P9,   KC_MPLY, KC_J,     KC_L,      KC_U,    KC_Y,    KC_SCLN,
-    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_PSCR, KC_P4,   KC_P5,   KC_P6,   KC_SLCK, KC_H,     KC_N,      KC_E,    KC_I,    KC_O,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,  KC_P1,   KC_P2,   KC_P3,   KC_HOME, KC_K,     KC_M,      KC_COMM, KC_DOT,  KC_SLSH,
-    KC_LCTL, KC_LGUI, KC_LALT, KC_EQL,  KC_VOLU, KC_DEL,  KC_NO,   KC_P0,   KC_PDOT, KC_END,  KC_VOLD,  KC_PGUP,   KC_RALT, KC_UP,   KC_RCTRL,
+    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_PSCR, KC_P4,   KC_P5,   KC_P6,   KC_HOME, KC_H,     KC_N,      KC_E,    KC_I,    KC_O,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SLCK, KC_P1,   KC_P2,   KC_P3,   KC_END,  KC_K,     KC_M,      KC_COMM, KC_DOT,  KC_SLSH,
+    KC_LCTL, KC_LGUI, KC_LALT, KC_EQL,  KC_VOLU, KC_ENT,  KC_NO,   KC_P0,   KC_PDOT, KC_BSPC, KC_VOLD,  KC_PGUP,   KC_RALT, KC_UP,   KC_RCTRL,
     KC_PAST, KC_COPY, MO(_FN), KC_MINS, KC_SPC,  KC_LSFT, MO(_SS), KC_GESC, MO(_SS), KC_RSFT, KC_SPC,   KC_PGDOWN, KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
