91a5d2a6bb4e1e47d591eef16eecdbdfbfd895a4
Julian T Skodzinski <juliantsk@gmail.com>
2019-05-14 22:02:49 -0400

    Fixed typo.

--- keyboards/xd75/keymaps/juliantsk/keymap.c
+++ keyboards/xd75/keymaps/juliantsk/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_CO] = LAYOUT_ortho_5x15( /* COLEMAK */
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_MINS, KC_P7,   KC_P8,   KC_P9,   KC_EQL,  KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
-    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_CPLK, KC_P4,   KC_P5,   KC_P6,   KC_MPLY, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
+    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_CAPS, KC_P4,   KC_P5,   KC_P6,   KC_MPLY, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PSCR, KC_P1,   KC_P2,   KC_P3,   KC_HOME, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
     KC_PSTE, KC_LGUI, KC_LCTRL,KC_LALT, KC_DEL,  KC_SLCK, KC_NO,   KC_P0,   KC_PDOT, KC_END,  KC_PGUP, KC_PGDN, KC_TAB,  KC_UP,   KC_ENT,
     KC_NO,   KC_COPY, MO(_FN), KC_LSFT, KC_SPC,  MO(_SS), KC_ENT, KC_GESC,  KC_BSPC, MO(_SS), KC_SPC,  KC_RSFT, KC_LEFT, KC_DOWN, KC_RIGHT
